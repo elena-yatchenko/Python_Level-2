b680b65ce7c7bbff672da97511cf3380e28e573c
Elena <hel.lena84@mail.ru>
2023-10-03 13:41:36 +0400

    HW 6 start

--- Homework_5/HW_5_Task_1.py
+++ Homework_5/HW_5_Task_1.py
@@ -10,16 +10,16 @@
 # ('C:/Users/User/Documents/', 'example', '.txt') 
 
 
-def get_file_info(file_path: str) -> tuple:
+# def get_file_info(file_path: str) -> tuple:
     
-    *a, b = file_path.split('/')
-    *c, d = b.split('.')
-    if file_path.count('/') > 0:
-        return f"{'/'.join(a)}/", f"{'.'.join(c)}", f".{d}" 
-    else:
-        return  f"{'/'.join(a)}", f"{'.'.join(c)}", f".{d}"
+#     *a, b = file_path.split('/')
+#     *c, d = b.split('.')
+#     if file_path.count('/') > 0:
+#         return f"{'/'.join(a)}/", f"{'.'.join(c)}", f".{d}" 
+#     else:
+#         return  f"{'/'.join(a)}", f"{'.'.join(c)}", f".{d}"
 
-print(get_file_info(file_path = 'file_in_current_directory.txt'))
+# print(get_file_info(file_path = 'file_in_current_directory.txt'))
 
 # file_path = '/home/user/docs/my.file.with.dots.txt'
 # file_path = 'file_in_current_directory.txt')
@@ -28,19 +28,31 @@ print(get_file_info(file_path = 'file_in_current_directory.txt'))
 # РЕШЕНИЕ СИСТЕМЫ: (НА ПРИМЕРЕ file_path = '/home/user/docs/my.file.with.dots.txt'))
 
 
-def get_file_info(file_path):
-    file_name = file_path.split("/")[-1] # взяли элемент с индексом [-1] получившегося словаря
-    print(file_path.split("/"))   # ['', 'home', 'user', 'docs', 'my.file.with.dots.txt']
-    print(file_name) # my.file.with.dots.txt
-    file_extension = file_name.split(".")[-1] 
-    print(file_name.split("."))  # ['my', 'file', 'with', 'dots', 'txt']
-    print(file_extension)  # txt
-    path = file_path[:-len(file_name)] # срез от начала строки до индекса, равного минус длина file_namе (-21)
-    print(path)   # /home/user/docs/
-    print(-len(file_name))  # -21
-    print(file_name[:-len(file_extension)-1]) # срез от начала строки file_name до элемента с индексом [-4], где доп. -1 - чтобы не включать точку (.txt) 
+# def get_file_info(file_path):
+#     file_name = file_path.split("/")[-1] # взяли элемент с индексом [-1] получившегося словаря
+#     print(file_path.split("/"))   # ['', 'home', 'user', 'docs', 'my.file.with.dots.txt']
+#     print(file_name) # my.file.with.dots.txt
+#     file_extension = file_name.split(".")[-1] 
+#     print(file_name.split("."))  # ['my', 'file', 'with', 'dots', 'txt']
+#     print(file_extension)  # txt
+#     path = file_path[:-len(file_name)] # срез от начала строки до индекса, равного минус длина file_namе (-21)
+#     print(path)   # /home/user/docs/
+#     print(-len(file_name))  # -21
+#     print(file_name[:-len(file_extension)-1]) # срез от начала строки file_name до элемента с индексом [-4], где доп. -1 - чтобы не включать точку (.txt) 
     
-    return (path, file_name[:-len(file_extension)-1], "." + file_extension)
+#     return (path, file_name[:-len(file_extension)-1], "." + file_extension)
 
-print(get_file_info(file_path = '/home/user/docs/my.file.with.dots.txt'))
+# print(get_file_info(file_path = '/home/user/docs/my.file.with.dots.txt'))
 
+# !!! решение с семинара (классное, но для нормально прописанных путей)
+
+def split_file_path(file_path):
+    # путь к директории, имя файла
+    dir_path, full_filename = file_path.rsplit("/", 1)
+
+    # имя файла, расширение
+    name, file_ext = full_filename.split(".", 1)
+    print(file_ext)
+    return dir_path, name, f'.{file_ext}'
+
+print(split_file_path(file_path = '/home/user/docs/my.file.with.dots.txt'))
