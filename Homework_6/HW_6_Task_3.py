5ad42cf89796a3379ddb3242d3aa268489d17a13
Elena <hel.lena84@mail.ru>
2023-10-04 18:09:22 +0400

    HW 6

--- Homework_6/HW_6_Task_3.py
+++ Homework_6/HW_6_Task_3.py
@@ -17,3 +17,44 @@
 # [[(1, 4), (2, 7), (3, 1), (4, 8), (5, 5), (6, 2), (7, 6), (8, 3)], [(1, 5), (2, 3), (
 # 3, 8), (4, 4), (5, 7), (6, 1), (7, 6), (8, 2)], [(1, 3), (2, 6), (3, 8), (4, 2), (5, 4), 
 # (6, 1), (7, 7), (8, 5)], [(1, 6), (2, 1), (3, 5), (4, 2), (5, 8), (6, 3), (7, 7), (8, 4)]]
+
+from itertools import combinations
+
+from random import randint, sample
+
+def is_attacking(q1, q2):
+    # Проверяем, бьют ли ферзи друг друга
+    return q1[0] == q2[0] or q1[1] == q2[1] or abs(q1[0] - q2[0]) == abs(q1[1] - q2[1])
+
+def check_queens(queens):
+    # Проверяем все возможные пары ферзей
+    for elem in combinations(queens, 2):
+        q1, q2 = elem
+        if is_attacking(q1, q2):
+            return False
+    return True
+
+def generate_boards():
+    my_list = [(randint(1, 8), randint(1, 8)) for i in range(16)]
+    queens = sample(my_list, 8)
+    return queens
+
+print(generate_boards())
+
+#def show_result():
+# board_list = []
+# n = 0
+# while n <= 4:
+#     if check_queens(generate_boards()) == True:
+#         board_list.append(generate_boards())
+#         n += 1
+    # return board_list
+# print(board_list)
+
+
+# print(check_queens(generate_boards()))
+# print(show_result())
+
+
+# print(generate_boards())
+
