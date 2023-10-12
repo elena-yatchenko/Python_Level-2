d027eff4659b4b1c471ef03cf5f0d7b167f3c5bc
Elena <hel.lena84@mail.ru>
2023-10-12 17:19:45 +0400

    additions

--- Homework_8/HW_8_Task_4(5).py
+++ Homework_8/HW_8_Task_4(5).py
@@ -16,14 +16,8 @@ def rename_json(directory):
             print(i)
             with open(i, 'r', encoding='utf-8') as f_read:
                 json_file = json.load(f_read)
-                for key,value in json_file.items():
-                    print(key, value)
-
-                # print(json_file)
-            # with open(f'{root} + .pickle', 'wb') as f_write:
-            #     pickle.dump(json_file, f_write)
+            with open(f'{root}.pickle', 'wb') as f_write:
+                pickle.dump(json_file, f_write)
 
 
 rename_json(r'C:\Users\User\Documents\PC_Data\Study\Python_Level-2\Homework_8')
-
-# ошибка
